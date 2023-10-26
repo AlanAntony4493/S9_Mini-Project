7d83e049c09e695eed3bdd130294dae7735fb05f
Alan <alanantony2024@gmail.com>
2023-10-26 08:48:27 +0530

    Mini Project Done, Selenium Testing Done

--- test.py
+++ test.py
@@ -1,81 +1,17 @@
-# from datetime import datetime
-# from django.test import TestCase
-# from selenium import webdriver
-# from selenium.webdriver.common.by import By
-# import time
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
-
-# class Hosttest(TestCase):
-#     def setUp(self):
-#         self.driver = webdriver.Chrome()
-#         self.driver.implicitly_wait(10)
-#         self.live_server_url = 'http://127.0.0.1:8000/'
-
-#     def tearDown(self):
-#         self.driver.quit()
-
-#     def test_02_registration_and_login(self):
-#         driver = self.driver
-#         driver.get(self.live_server_url)
-#         driver.maximize_window()
-#         time.sleep(1)
-
-#         login_link = driver.find_element(By.CSS_SELECTOR, "#login")
-#         login_link.click()
-#         time.sleep(2)
-
-#         username = driver.find_element(By.CSS_SELECTOR, "input#username")
-#         username.send_keys("alanantony96696@gmail.com")
-#         password = driver.find_element(By.CSS_SELECTOR, "input#password")
-#         password.send_keys("@Lan4493")
-#         time.sleep(1)
-
-#         submit_button = driver.find_element(By.CSS_SELECTOR, "button#submit")
-#         submit_button.click()
-#         time.sleep(2)
-
-#         # Navigate to the career forum page
-#         driver.get("http://127.0.0.1:8000/career_forum")
-#         time.sleep(2)
-
-#         # Locate and fill out the form fields
-#         title_field = driver.find_element(By.ID, "questionTitle")
-#         title_field.send_keys("How are you doing hai")
-
-#         description_field = driver.find_element(By.ID, "questionDescription")
-#         description_field.send_keys("Steps Summarize your problem in a one-line title. Describe your problem in more detail.Include any additional details.Review your question and post it to the site. You’re ready to ask a career-related question, and this form will help guide you through the process.")
-#         additional_details_field = driver.find_element(By.ID, "additionalDetails")
-#         additional_details_field.send_keys("Additional details for the question ggg.")
-#         time.sleep(1)
-
-#         submit_button = driver.find_element(By.CSS_SELECTOR, "input#sub")
-#         submit_button.click()
-#         # time.sleep(2)
-
-#         # submitc = driver.find_element(By.CSS_SELECTOR, "a#logout")
-#         # submitc.click()
-#         # time.sleep(2)
-
-#         print("Test done successfully")
-
-# if __name__ == '_main_':
-#     import unittest
-#     unittest.main()
-
-
-
-from datetime import datetime
 from django.test import TestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
 import time
 
 class Hosttest(TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome()
         self.driver.implicitly_wait(10)
-        self.live_server_url = 'http://127.0.0.1:8000/'
+        self.live_server_url = 'http://127.0.0.1:8000/register'
 
     def tearDown(self):
         self.driver.quit()
@@ -86,49 +22,47 @@ class Hosttest(TestCase):
         driver.maximize_window()
         time.sleep(1)
 
-        # Navigate to the user index page
-        driver.get(self.live_server_url + '/index')
-        time.sleep(2)
+        # Fill out the registration form
+        fname = driver.find_element(By.ID, "fname")
+        fname.send_keys("John")
+        mname = driver.find_element(By.ID, "Mname")
+        mname.send_keys("Middle")
+        lname = driver.find_element(By.ID, "lname")
+        lname.send_keys("Doe")
+        # lname = driver.find_element(By.ID, "lname")
+        # lname.send_keys("Doe")
+        house_name = driver.find_element(By.ID, "Hname")
+        house_name.send_keys("Main Street")
 
-        # Click on the login link
-        login_link = driver.find_element(By.CSS_SELECTOR, "#login")
-        login_link.click()
-        time.sleep(2)
+        # Select the prayer group (e.g., 'Gethsemane')
+        prayer_group_dropdown = Select(driver.find_element(By.ID, "prayer-group"))
+        prayer_group_dropdown.select_by_value("Gethsemane")
 
-        # Navigate to the login page
-        login_page_url = driver.current_url
+        dob = driver.find_element(By.ID, "dob")
+        dob.send_keys("21-10-2001")
 
-        # Enter credentials (email and password)
-        username = driver.find_element(By.CSS_SELECTOR, "input#username")
-        username.send_keys("SmymAdmin@2023")
-        password = driver.find_element(By.CSS_SELECTOR, "input#password")
-        password.send_keys("@lan4493")
-        time.sleep(1)
+        # Select the gender (e.g., 'Male')
+        gender_dropdown = Select(driver.find_element(By.ID, "gender"))
+        gender_dropdown.select_by_value("male")
 
-        submit_button = driver.find_element(By.CSS_SELECTOR, "button#submit")
+        email = driver.find_element(By.ID, "email")
+        email.send_keys("john123@example.com")
+
+        password = driver.find_element(By.ID, "password")
+        password.send_keys("@jhon1234")
+
+        confirm_password = driver.find_element(By.ID, "cpassw")
+        confirm_password.send_keys("@jhon1234")
+
+        phone = driver.find_element(By.ID, "mob")
+        phone.send_keys("8746353727")
+
+        submit_button = driver.find_element(By.XPATH, "//button[contains(text(), 'Sign Up')]")
         submit_button.click()
         time.sleep(2)
 
-        # Check if successfully redirected to the home page
-        assert driver.current_url != login_page_url, "Login failed"
-
-        # Click on the "Users" button
-        users_button = driver.find_element(By.CSS_SELECTOR, "#users")
-        users_button.click()
-        time.sleep(2)
-
-        # Click on the "Approval" button
-        approval_button = driver.find_element(By.CSS_SELECTOR, "#activate")
-        approval_button.click()
-        time.sleep(2)
-
-        # Click on the "Logout" button
-        logout_button = driver.find_element(By.CSS_SELECTOR, "#logout-button")
-        logout_button.click()
-        time.sleep(2)
-
-        # Check if successfully logged out
-        assert "Login" in driver.page_source, "Logout failed"
+        
+        print("Testing Success")
 
 if __name__ == '__main__':
     import unittest
